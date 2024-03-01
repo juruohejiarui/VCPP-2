d1f4bdc0f4e66838027096f8b797da7d1639f578
juruohejiarui <juruohjr@gmail.com>
2024-03-01 11:37:34 +0800

    TEMP

--- VGVM/syscall.c
+++ VGVM/syscall.c
@@ -7,21 +7,24 @@ void *syscall[] = {VMputchar, VMgetchar, printInt, inputInt, printInt64, inputIn
 void VMputchar(char ch) { putchar(ch); }
 char VMgetchar() { return getchar(); }
 void printInt(int32 data) {
-AlignRsp
+    AlignRsp
     printf("%d", data); }
 int32 inputInt() { 
-AlignRsp
+    AlignRsp
     int32 data; scanf("%d", &data); return data; 
 }
 void printInt64(int64 data) {
     AlignRsp
     printf("%lld", data);
 }
-int64 inputInt64() { int64 data; scanf("%lld", &data); return data; }
+int64 inputInt64() { 
+    AlignRsp
+    int64 data; scanf("%lld", &data); return data; 
+}
 void printUInt64(uint64 data) {
     AlignRsp
     printf("%llu", data); 
-    }
+}
 uint64 inputUInt64() { uint64 data;
     AlignRsp
     scanf("%llu", &data); return data; 
@@ -32,4 +35,3 @@ void printFloat32(uint64 data) {
     AlignRsp
     printf("%.10f", *(float32 *)&data); 
 }
-
