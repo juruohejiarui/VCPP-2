aeff6ea547166e38b41fed5cae34293e18020d66
juruohejiarui <1466196957@qq.com>
2024-09-06 10:54:48 +0800

    working on HID

--- VOS/kernel/hardware/USB/HID/desc.h
+++ VOS/kernel/hardware/USB/HID/desc.h
@@ -17,31 +17,40 @@ struct USB_HID_Driver {
 };
 
 #pragma region Report Parse
-#define HID_ReportItem_Tag	(0xfc)
-#define HID_ReportItem_Type	(0x02)
-#define HID_ReportItem_Type_Main	(0 << 2)
-#define HID_ReportItem_Type_Global	(1 << 2)
-#define HID_ReportItem_Type_Local	(2 << 2)
-#define HID_ReportItem_Size	(0x03)
+#define HID_RepItem_Size	(0x03)
+#define HID_RepItem_Type	(0x0c)
 
-#define HID_ReportItem_DataType_Const 		(1 << 0)
-#define HID_ReportItem_DataType_Vari		(1 << 1)
-#define HID_ReportItem_DataType_Relative	(1 << 2)
-#define HID_ReportItem_DataType_Wrap		(1 << 3)
-#define HID_ReportItem_DataType_NoLinear	(1 << 4)
-#define HID_ReportItem_DataType_NoPrefered	(1 << 5)
-#define HID_ReportItem_DataType_NullState	(1 << 6)
+#define HID_RepItem_Type_Main	(0x0)
+#define HID_RepItem_Type_Global	(0x1)
+#define HID_RepItem_Type_Local	(0x2)
+#define HID_RepItem_Type_Long	(0x3)
+
+#define HID_RepItem_Tag		(0xf0)
+
+#define HID_RepItem_Tag_Input	0x08
+#define HID_RepItem_Tag_Output	0x09
+#define HID_RepItem_Tag_Coll	0x0a
+#define HID_RepItem_Tag_Feature	0x0b
+#define HID_RepItem_Tag_EndColl	0x0c
+
+#define HID_RepItem_DataType_Const 		(1 << 0)
+#define HID_RepItem_DataType_Vari		(1 << 1)
+#define HID_RepItem_DataType_Relative	(1 << 2)
+#define HID_RepItem_DataType_Wrap		(1 << 3)
+#define HID_RepItem_DataType_NoLinear	(1 << 4)
+#define HID_RepItem_DataType_NoPrefered	(1 << 5)
+#define HID_RepItem_DataType_NullState	(1 << 6)
 
 #pragma endregion
 
 typedef struct USB_HID_ReportItem {
-	int rgMin, rgMx, off;
+	int rgMn, rgMx, off;
 	u8 flags, size;
 	// offset == -1 means this item does not exist in a report
 } USB_HID_ReportItem;
 
 typedef struct USB_HID_ReportHelper {
-	int type, totSz;
+	int type, inSz, outSz;
 	// the raw data of report descriptor
 	u8 *raw;
 	#define USB_HID_ReportHelper_Type_Mouse 1
@@ -50,12 +59,13 @@ typedef struct USB_HID_ReportHelper {
 	#define USB_HID_ReportParseHelper_Type_Mouse 0
 	union {
 		struct {
-			USB_HID_ReportItem btn, mvX, mvY, mxZ;
+			USB_HID_ReportItem btn[3], mvX, mvY, mvZ;
 		} mouse;
 		union {
 			USB_HID_ReportItem spK, k1, k2, k3, k4, k5, k6;
+			USB_HID_ReportItem leds;
 		} keyboard;
-	};
+	} items;
 } __attribute__ ((packed)) USB_HID_ReportHelper;
 extern struct USB_HID_Driver HW_USB_HID_driver;
 
--- VOS/kernel/hardware/USB/HID/parse.c
+++ VOS/kernel/hardware/USB/HID/parse.c
@@ -1,47 +1,94 @@
 #include "api.h"
 #include "../../../includes/log.h"
 
-static u32 _getData(u8 *report, u64 itemSize) {
-    u32 data = report[0];
-    if (itemSize > 1) {
-        data |= ((u16)report[1]) << 8;
-        if (itemSize > 2) {
-            data |= ((u32)report[2]) << 16;
-            if (itemSize > 3) data |= ((u32)report[3]) << 24;
-        }
-    }
-    return data;
+static __always_inline__ int _getPrefixField(u8 prefix, u32 mask) { return (prefix & mask) >> (Bit_ffs(mask) - 1); }
+static __always_inline__ int _getItemSize(u8 *rep) {
+    u8 *szField = _getPrefixField(*rep, HID_RepItem_Size);
+    return szField + (szField == 3 ? 1 : 0);
 }
 
-void _parseMain(USB_HID_ReportHelper *helper, u32 tag, u32 data, u64 usageFlags, int curSize, int curCnt, int curMn, int curMx) {
+struct DataState {
+    int lgMn, lgMx, phyMn, phyMx;
+    int cnt, sz;
+    u8 usageLs[16], usageLsL;  
+};
+
+static void (*_helperModifier[0x100])(struct DataState *, u8, USB_HID_ReportHelper *helper);
+
+static void _applyItem(struct DataState *state, u8 flags, int isIn, USB_HID_ReportHelper *helper, USB_HID_ReportItem *item) {
+    item->flags = flags;
+    item->rgMn = state->lgMn;
+    item->rgMx = state->lgMx;
+    if (isIn) item->off = helper->inSz, helper->inSz += state->sz;
+    else item->off = helper->outSz, helper->outSz += state->sz;
+}
+
+static void _helperModifier_X(struct DataState *state, u8 flags, USB_HID_ReportHelper *helper) {
+    if (helper->type != USB_HID_ReportHelper_Type_Mouse) return ;
+    _applyItem(state, flags, 1, helper, &helper->items.mouse.mvX);
+}
+static void _helperModifier_Y(struct DataState *state, u8 flags, USB_HID_ReportHelper *helper) {
+    if (helper->type != USB_HID_ReportHelper_Type_Mouse) return ;
+    _applyItem(state, flags, 1, helper, &helper->items.mouse.mvY);
+}
+static void _helperModifier_Z(struct DataState *state, u8 flags, USB_HID_ReportHelper *helper) {
+    if (helper->type != USB_HID_ReportHelper_Type_Mouse) return ;
+    _applyItem(state, flags, 1, helper, &helper->items.mouse.mvZ);
+}
+static void _helperModifer_btn(struct DataState *state, u8 flags, USB_HID_ReportHelper *helper) {
+    if (helper->type == USB_HID_ReportHelper_Type_Mouse) {
+        for (int i = 0; i < 3; i++) if (!helper->items.mouse.btn[i].size) {
+            _applyItem(state, flags, 1, helper, &helper->items.mouse.btn[i]);
+            break;
+        }
+    }
+}
+
+void _applyHelper(USB_HID_ReportHelper *helper, u8 flags, struct DataState *state) {
     
 }
 
-USB_HID_ReportHelper *HW_USB_HID_genParseHelper(u8 *report, u64 len) {
-    USB_HID_ReportHelper *helper = kmalloc(sizeof(USB_HID_ReportHelper), Slab_kmalloc_arg_Private | Slab_kmalloc_arg_Clear, NULL);
-    helper->raw = report;
+int *_parseMain(USB_HID_ReportHelper *helper, u8 *rep, struct DataState *state) {
+    switch (_getPrefixField(*rep, HID_RepItem_Tag)) {
+        case HID_RepItem_Tag_Input:
+        case HID_RepItem_Tag_Output:
+            _createHelper(helper, *(rep + 1), state);
+            break;
+        case HID_RepItem_Tag_Feature:
+            // no support for this tag;
+            break;
+        case HID_RepItem_Tag_Coll:
+        
+        case HID_RepItem_Tag_EndColl:
+
+    }
+}
+
+USB_HID_ReportHelper *HW_USB_HID_genParseHelper(u8 *rep, u64 len) {
     u64 idx = 0;
-    int curSize = 0, curCnt = 0;
-    int curMn = -32768, curMx = 32768;
-    u64 usageFlags;
+    int res;
+    USB_HID_ReportHelper *helper = kmalloc(sizeof(USB_HID_ReportHelper), Slab_kmalloc_arg_Private | Slab_kmalloc_arg_Clear, NULL);
+    struct DataState curDtState;
+    memset(&curDtState, 0, sizeof(struct DataState));
     while (idx < len) {
-        int itemType = report[idx] & HID_ReportItem_Tag;
-        int itemSize = (report[idx] & HID_ReportItem_Size) ? (report[idx] & HID_ReportItem_Size) : 4;
-        if (idx + itemSize > len) {
-            printk(RED, BLACK, "HID: syntax error on report %#018lx\n", report);
+        int itemLen = _getItemSize(*(rep + idx));
+        switch (_getPrefixField(*(rep + idx), HID_RepItem_Type)) {
+            case HID_RepItem_Type_Main:
+                res = _parseMain(helper, rep + idx, &curDtState);
+                break;
+            case HID_RepItem_Type_Global:
+                break;
+            case HID_RepItem_Type_Local:
+                break;
+            default :
+                printk(RED, BLACK, "HID parse helper: No support for long item.\n");
+                return helper;
+        }
+        if (!res) {
             helper->type = 0;
             return helper;
         }
-        u32 data = _getData(report + idx + 1, itemSize);
-        switch (itemType & HID_ReportItem_Type) {
-            case HID_ReportItem_Type_Main:
-                _parseMain(helper, itemType, data, usageFlags, curSize, curCnt, curMn, curMx);
-                break;
-            case HID_ReportItem_Type_Global:
-                break;
-            case HID_ReportItem_Type_Local:
-                break;
-        }
+        idx += 1 + itemLen;
     }
     return helper;
 }
